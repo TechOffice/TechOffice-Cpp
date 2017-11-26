659a220a8c181b893338ec21e7d1781022501df5
imben1109 <imben1109@gmail.com>
2017-11-26 20:06:43 +0800

    update'

---
+++ 040-Tools/010-VisualStudio/HelloWorldExample/HelloWorldExample/HelloWorldExample.cpp
@@ -0,0 +1,13 @@
+// HelloWorldExample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
